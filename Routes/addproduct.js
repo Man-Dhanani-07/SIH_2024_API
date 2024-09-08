5f304b7a80da2302b4d6308d14031f019a27c6ae
Man Dhanani <mandhanani96@gmail.com>
2024-09-08 13:24:15 +0530

    change update & delete

--- Routes/addproduct.js
+++ Routes/addproduct.js
@@ -26,7 +26,7 @@ router.get("/get-product", async (req, res) => {
 router.post("/post-product", upload.any(), async (req, res) => {
     const image = req.files ? req.files.map(file => file.path) : [];
     const newProduct = new Product({
-    product_id:req.body.product_id,
+    product_id:req.body.Product_id,
     productname: req.body.productname,
     category: req.body.category,
     subcategory: req.body.subcategory,
@@ -64,25 +64,24 @@ router.delete("/delete-product/:product_id", async (req, res) => {
 });
 router.put("/update-product/:product_id", upload.any(), async (req, res) => {
   const updateData = {
-    product_id:req.body.product_id,
-    productname: req.body.productname,
-    category: req.body.category,
-    subcategory: req.body.subcategory,
-    quantity: req.body.quantity,
-    price: req.body.price,
-    date: req.body.date,
-    description: req.body.description,
-    address: req.body.address,
-    sellerdetails: {
-      seller_id: req.body.sellerdetails.seller_id,
-      seller_name: req.body.sellerdetails.seller_name,
-      seller_city: req.body.sellerdetails.seller_city,
-      seller_contact_no: req.body.sellerdetails.seller_contact_no,
-      seller_email: req.body.sellerdetails.seller_email,
-      seller_language: req.body.sellerdetails.seller_language,
-      seller_address: req.body.sellerdetails.seller_address,
-      seller_occupation: req.body.sellerdetails.seller_occupation
-    }
+    productname: req.body.productname || Product.productname,
+    category: req.body.category || Product.category,
+    subcategory: req.body.subcategory || Product.subcategory,
+    quantity: req.body.quantity || Product.quantity,
+    price: req.body.price || Product.price,
+    date: req.body.date || Product.date,
+    description: req.body.description || Product.description,
+    address: req.body.address || Product.address,
+    sellerdetails: req.body.sellerdetails ? {
+      seller_id: req.body.sellerdetails.seller_id || Product.sellerdetails.seller_id,
+      seller_name: req.body.sellerdetails.seller_name || Product.sellerdetails.seller_name,
+      seller_city: req.body.sellerdetails.seller_city || Product.sellerdetails.seller_city,
+      seller_contact_no: req.body.sellerdetails.seller_contact_no || Product.sellerdetails.seller_contact_no,
+      seller_email: req.body.sellerdetails.seller_email || Product.sellerdetails.seller_email,
+      seller_language: req.body.sellerdetails.seller_language || Product.sellerdetails.seller_language,
+      seller_address: req.body.sellerdetails.seller_address || Product.sellerdetails.seller_address,
+      seller_occupation: req.body.sellerdetails.seller_occupation || Product.sellerdetails.seller_occupation
+    }:Product.sellerdetails
   };
 if (req.files && req.files.length > 0) {
     updateData.image = req.files.map(file => file.path);
@@ -92,7 +91,8 @@ if (req.files && req.files.length > 0) {
       { product_id: req.params.product_id },
       { $set: updateData }
     );
-    res.json(data);
+    const updatedProduct = await Product.findOne({ product_id: req.params.product_id });
+    res.json(updatedProduct);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
