e2ec539952ea2230278190344a0a7a76f4319e31
Man Dhanani <mandhanani96@gmail.com>
2024-09-08 16:37:30 +0530

    aa

--- Routes/addproduct.js
+++ Routes/addproduct.js
@@ -3,17 +3,6 @@ const router = express.Router();
 const multer = require("multer");
 const Product = require("../Models/data");
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, './uploads');
-//   },
-//   filename: function (req, file, cb) {
-//     let filename = new Date().getTime().toString(36); 
-//     let extension = file.originalname.split(".").pop(); 
-//     cb(null, `${filename}.${extension}`);
-//   },
-// });
-// const upload = multer({ storage: storage });
 
 router.get("/get-product", async (req, res) => {
   try {
@@ -33,37 +22,6 @@ router.post("/post-product", async (req, res) => {
       console.error("Error saving the product:", error);
       res.status(500).send({ message: "Failed to save the product", error });
   }
-    // let data = new Product(req.body);
-    // let result = await data.save();
-    // res.send(result);
-    // const newProduct = new Product({
-    // product_id:req.body.product_id,
-    // productname: req.body.productname,
-    // category: req.body.category,
-    // subcategory: req.body.subcategory,
-    // quantity: req.body.quantity,
-    // price: req.body.price,
-    // date: req.body.date,
-    // description: req.body.description,
-    // address: req.body.address,
-    // image:image,
-    // sellerdetails: {
-    //   seller_id: req.body.sellerdetails.seller_id,
-    //   seller_name: req.body.sellerdetails.seller_name,
-    //   seller_city: req.body.sellerdetails.seller_city,
-    //   seller_contact_no: req.body.sellerdetails.seller_contact_no,
-    //   seller_email: req.body.sellerdetails.seller_email,
-    //   seller_language: req.body.sellerdetails.seller_language,
-    //   seller_address: req.body.sellerdetails.seller_address,
-    //   seller_occupation: req.body.sellerdetails.seller_occupation
-    // }
-  // });
-  // try {
-  //   let result = await newProduct.save();
-  //   res.json(result);
-  // } catch (err) {
-  //   res.status(400).json({ error: err.message });
-  // }
 });
 router.delete("/delete-product/:product_id", async (req, res) => {
   try {
@@ -114,28 +72,6 @@ router.put("/update-product/:product_id", async (req, res) => {
   res.status(400).json({ error: err.message });
 }
 });
-// router.get("/by-category/:category", async (req, res) => {
-//   try {
-//     const category = req.params.category;
-//     const products = await Product.find({ category: category });
-//     if (products.length === 0) {
-//       return res.status(404).json({ message: "No products found in this category" });
-//     }
-//     res.json(products);
-//   } catch (err) {
-//     res.status(400).json({ error: err.message });
-//   }
-// });
-  // try {
-  //   let data = await Product.updateOne(
-  //     { product_id: req.params.product_id },
-  //     { $set: updateData }
-  //   );
-  //   // const updatedProduct = await Product.findOne({ product_id: req.params.product_id });
-  //   res.json(updatedProduct);
-  // } catch (err) {
-  //   res.status(400).json({ error: err.message });
-  // }
 router.get("/by-category/:category", async (req, res) => {
   try {
     const category = req.params.category;
